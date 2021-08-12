2dcc2c8a9c3d172f5b95837fc6976bc6f619547c
Anthony Lopez <grungekarma@gmail.com>
2021-08-12 15:11:17 -0500

    global object

---
+++ theGlobalObject/index.js
@@ -0,0 +1,9 @@
+//this is a global object
+var a = "hello world";
+
+//this function is also a global object
+function b () {
+
+}
+// this and window in the console will bring up the same window object
+//ex. 'this.a' returns the string 'hello world' and this.b returns the function
