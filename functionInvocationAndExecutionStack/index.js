ba247e232d780dbad0cd6762065a8c78ed9e9b31
Anthony Lopez <grungekarma@gmail.com>
2021-08-13 12:21:12 -0500

    function invocation and execution stack

---
+++ functionInvocationAndExecutionStack/index.js
@@ -0,0 +1,24 @@
+function b() {
+	var myVar;
+	  //expect output to be 'undefined'
+    console.log(myVar);
+}
+
+function a() {
+	var myVar = 2;
+		//expect output to be 2
+    console.log(myVar);
+	b();
+}
+
+var myVar = 1;
+//expect output to be 1
+console.log(myVar);
+a();
+//expect output to be 1
+console.log(myVar);
+
+//output will be "1 2 undefined 1"
+
+//every function creates a new execution context
+//once executed the execution context is popped off the stack
