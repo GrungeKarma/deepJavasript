cd21b5a092d861379955f63b41f32a68c17b71a7
Anthony Lopez <grungekarma@gmail.com>
2021-08-21 11:34:27 -0500

    add linter

--- section3-objectsAndFunctions/002-objectsAndObjectLiterals/index.js
+++ section3-objectsAndFunctions/002-objectsAndObjectLiterals/index.js
@@ -1,1 +1,33 @@
+// var person = { firstname: 'Anthony', lastname: 'Lopez' };
+// console.log(person);
+//the code above is the same as
+// person = newObject();
+// person.firstname = 'Anthony';
+// person.lastname = 'Lopez';
+
+var Anthony = { //treated as one line of code
+  firstname: 'Anthony',
+  lastname: 'Lopez',
+  address: {
+    street: '123 Main St.',
+    state: 'NH',
+    country: 'USA'
+  }
+};
+
+function greet(person) {
+  console.log('Hi ' + person.firstname);
+}
+
+//console logs entire object
+console.log(Anthony);
+//logs Hi Anthony
+greet(Anthony);
+
+//you can input objects directly into the function
+//logs Hi Gary
+greet({ firstname: 'Gary', lastname: 'Mitz' });
+
+
+
 
